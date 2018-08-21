362cce8ee6fa5e127292b15d0a63cf2c2bacf962
smridhisharma <smridhisharma421@gmail.com>
2018-08-21 09:53:17 +0530

    Assig 2

---
+++ assign2.py
@@ -0,0 +1,30 @@
+#Q1
+a="smridhi sharma"
+print(a)
+
+#Q2
+a="hello friend"
+print('hello' + 'you')
+
+#Q3
+a=int(input("enter value of a"))
+b=int(input("enter value of b"))
+c=int(input("enter value of c"))
+print(a)
+print(b)
+print(c)
+
+#Q4
+a="let's get started"
+print(a)
+
+#Q5
+a="my"
+b="job"
+sal=80000
+print("%s %s sal is %d."%(a,b,sal))
+
+#Q6
+rad=int(input("enter  radius"))
+a=3.14*rad*rad
+print(a)
